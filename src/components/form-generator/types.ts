a484e6d178771680fafca1b4821a5a6a881231ee
Valeria Kartasheva <valeriakartaseva39@gmail.com>
2026-05-27 18:42:44 +0300

    feat: add form-generator TypeScript types

---
+++ src/components/form-generator/types.ts
@@ -0,0 +1,29 @@
+export interface IBaseField {
+  label: string
+  model: string
+  required?: boolean
+  pattern?: string
+}
+
+export interface ITextField extends IBaseField {
+  type: 'text' | 'email' | 'password'
+  minLength?: number
+}
+
+export interface ISelectField extends IBaseField {
+  type: 'select'
+  options: string[]
+}
+
+export interface ICheckboxField extends IBaseField {
+  type: 'checkbox'
+}
+
+export type TFieldSchema = ITextField | ISelectField | ICheckboxField
+
+export interface IFormSchema {
+  fields: TFieldSchema[]
+}
+
+export type TFormData = Record<string, string | boolean>
+export type TFormErrors = Record<string, string>
